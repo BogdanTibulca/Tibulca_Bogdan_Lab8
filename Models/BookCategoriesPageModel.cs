32c808c517b27f4705760eacfed2232bdf287d63
BogdanTibulca <bogdantibulca@yahoo.com>
2021-11-20 11:06:35 +0200

    added branch for Lab9

---
+++ Models/BookCategoriesPageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tibulca_Bogdan_Lab8.Data;
+
+namespace Tibulca_Bogdan_Lab8.Models
+{
+    public class BookCategoriesPageModel : PageModel
+    {
+        public List<AssignedCategoryData> AssignedCategoryData;
+
+        public void PopulateAssignedCategoryData(Tibulca_Bogdan_Lab8Context context, Book Book)
+        {
+
+        }
+    }
+}
