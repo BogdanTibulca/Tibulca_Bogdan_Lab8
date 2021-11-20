8e3ad218d07af924c71c78924daa962b52496075
BogdanTibulca <bogdantibulca@yahoo.com>
2021-11-20 11:44:21 +0200

    updated the app with the features from lab 9

--- Models/BookCategoriesPageModel.cs
+++ Models/BookCategoriesPageModel.cs
@@ -10,11 +10,64 @@ namespace Tibulca_Bogdan_Lab8.Models
 {
     public class BookCategoriesPageModel : PageModel
     {
-        public List<AssignedCategoryData> AssignedCategoryData;
+        public List<AssignedCategoryData> AssignedCategoryDataList;
 
         public void PopulateAssignedCategoryData(Tibulca_Bogdan_Lab8Context context, Book Book)
         {
+            var allCategories = context.Category;
+            var bookCategories = new HashSet<int>(
+                Book.BookCategories.Select(c => c.CategoryID));
 
+            AssignedCategoryDataList = new List<AssignedCategoryData>();
+
+            foreach (var cat in allCategories)
+            {
+                AssignedCategoryDataList.Add(new AssignedCategoryData
+                {
+                    CategoryID = cat.ID,
+                    Name = cat.CategoryName,
+                    Assigned = bookCategories.Contains(cat.ID)
+                });
+            }
+        }
+
+        public void UpdateBookCategories(Tibulca_Bogdan_Lab8Context context,
+            string[] selectedCategories, Book bookToUpdate)
+        {
+            if (selectedCategories == null)
+            {
+                bookToUpdate.BookCategories = new List<BookCategory>();
+                return;
+            }
+
+            var selectedCategoriesHS = new HashSet<string>(selectedCategories);
+            var bookCategories = new HashSet<int>(bookToUpdate.BookCategories.Select(c => c.Category.ID));
+
+            foreach (var cat in context.Category)
+            {
+                if (selectedCategoriesHS.Contains(cat.ID.ToString()))
+                {
+                    if (!bookCategories.Contains(cat.ID))
+                    {
+                        bookToUpdate.BookCategories.Add(
+                            new BookCategory
+                            {
+                                BookID = bookToUpdate.ID,
+                                CategoryID = cat.ID
+                            });
+                    }
+                }
+                else
+                {
+                    if (bookCategories.Contains(cat.ID))
+                    {
+                        BookCategory courseToRemove = bookToUpdate
+                            .BookCategories
+                            .SingleOrDefault(i => i.CategoryID == cat.ID);
+                        context.Remove(courseToRemove);
+                    }
+                }
+            }
         }
     }
 }
