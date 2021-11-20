084726b6b33ae0e7ffb3ec2b5da59228ea716726
BogdanTibulca <bogdantibulca@yahoo.com>
2021-11-20 10:18:44 +0200

    Updated the Lab8. All functionality is working.

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tibulca_Bogdan_Lab8.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public String CategoryName { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+
+
+    }
+}
